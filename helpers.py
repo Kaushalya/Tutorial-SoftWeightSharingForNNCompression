c06fa5e49f494617456071699b44c23383201726
Kaushalya <kau.mad@gmail.com>
2017-02-21 21:18:39 +0900

    fixing compile errors for python3

--- helpers.py
+++ helpers.py
@@ -40,7 +40,7 @@ def reshape_like(in_array, shaped_array):
 
 def merger(inputs):
     """Comparing and merging components."""
-    for _ in xrange(3):
+    for _ in range(3):
         lists = []
         for inpud in inputs:
             for i in inpud:
@@ -70,7 +70,7 @@ def compute_responsibilies(xs, mus, logprecisions, pis):
     K = len(pis)
     W = len(xs)
     responsibilies = np.zeros((K, len(xs)))
-    for k in xrange(K):
+    for k in range(K):
         # Not normalized!!!
         responsibilies[k] = pis[k] * np.exp(0.5 * logprecisions[k]) * np.exp(
             - np.exp(logprecisions[k]) / 2 * (xs - mus[k]) ** 2)
@@ -122,21 +122,20 @@ def discretesize(W, pi_zero=0.999):
 
 
 def save_histogram(W_T,save, upper_bound=200):
-        w = np.squeeze(special_flatten(W_T[:-3]))
-        plt.figure(figsize=(10, 7))
-        sns.set(color_codes=True)
-        plt.xlim(-1,1)
-        plt.ylim(0,upper_bound)
-        sns.distplot(w, kde=False, color="g",bins=200,norm_hist=True)
-        plt.savefig("./"+save+".png", bbox_inches='tight')
-        plt.close()
+   w = np.squeeze(special_flatten(W_T[:-3]))
+   plt.figure(figsize=(10, 7))
+   sns.set(color_codes=True)
+   plt.xlim(-1,1)
+   plt.ylim(0,upper_bound)
+   sns.distplot(w, kde=False, color="g",bins=200,norm_hist=True)
+   plt.savefig("./"+save+".png", bbox_inches='tight')
+   plt.close()
 
-
-        plt.figure(figsize=(10, 7))
-	plt.yscale("log")
-        sns.set(color_codes=True)
-        plt.xlim(-1,1)
-        plt.ylim(0.001,upper_bound*5)
-        sns.distplot(w, kde=False, color="g",bins=200,norm_hist=True)
-        plt.savefig("./"+save+"_log.png", bbox_inches='tight')
-        plt.close()
+   plt.figure(figsize=(10, 7))
+   plt.yscale("log")
+   sns.set(color_codes=True)
+   plt.xlim(-1,1)
+   plt.ylim(0.001,upper_bound*5)
+   sns.distplot(w, kde=False, color="g",bins=200,norm_hist=True)
+   plt.savefig("./"+save+"_log.png", bbox_inches='tight')
+   plt.close()
